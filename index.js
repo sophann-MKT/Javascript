0ca37b0b668496972cea9d8ec3537f3899ccfd83
sophann <sophann.morakot@gmail.com>
2023-11-17 14:44:30 +0700

    changes

--- index.js
+++ index.js
@@ -495,10 +495,10 @@
 
 // the spread operator
 
-const q1 = ["Jan", "Feb", "Mar"];
-const q2 = ["Apr", "May", "Jun"];
-const q3 = ["Jul", "Aug", "Sep"];
-const q4 = ["Oct", "Nov", "May"];
+// const q1 = ["Jan", "Feb", "Mar"];
+// const q2 = ["Apr", "May", "Jun"];
+// const q3 = ["Jul", "Aug", "Sep"];
+// const q4 = ["Oct", "Nov", "May"];
 
-const year = [...q1, ...q2, ...q3, ...q4];
-console.table(year);
+// const year = [...q1, ...q2, ...q3, ...q4];
+// console.table(year);
